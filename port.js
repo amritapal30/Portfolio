bd4bdc5c341bf0ab4b027ef0888190438753b604
Amrita  Pal <145271835+amritapal30@users.noreply.github.com>
2023-10-04 12:35:29 +0530

    Update port.js

--- port.js
+++ port.js
@@ -50,5 +50,3 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p .about-content', { origin: 'right' });
-
-/*==================== typed js ====================*/
